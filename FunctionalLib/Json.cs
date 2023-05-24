7a54c0bc365f8d963dbb61cdb1392f44af76b4f4
topototty <myshmyshevskaya@gmail.com>
2023-05-24 22:41:41 +0300

    aaa

---
+++ FunctionalLib/Json.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalLib
+{
+    public class Json
+    {
+        public static void Serialize<T>(string fileName, T obj)
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if(!File.Exists(desktop + "\\" + fileName))
+            {
+                FileStream fileStream = File.Create(desktop + "\\" + fileName);
+                fileStream.Dispose();
+            }
+
+            var json = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(desktop + "\\" + fileName, json);
+        }
+
+        public static T Deserialize<T>(string fileName)
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (!File.Exists(desktop + "\\" + fileName))
+            {
+                FileStream fileStream = File.Create(desktop + "\\" + fileName);
+                fileStream.Dispose();
+            }
+
+            string json = File.ReadAllText(desktop + "\\" + fileName);
+            T obj = JsonConvert.DeserializeObject<T>(json);
+            return obj;
+        }
+    }
+}
---
+++ Libss/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FunctionalLib;
+
+namespace Libss
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Serialize_click(object sender, RoutedEventArgs e)
+        {
+            Person personKasha = new Person
+            {
+                Name = "Каша",
+                Age = 17
+            };
+
+            Json.Serialize("person.json", personKasha);
+        }
+
+        private void Deserialize_click(object sender, RoutedEventArgs e)
+        {
+            Json.Deserialize<Person>("person.json");
+        }
+    }
+}
